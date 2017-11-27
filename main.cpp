14a3b41116ff742cfa72baec2225c0f58c701320
Hunter Mayers <HunterCMayers@gmail.com>
2017-11-26 16:39:08 -0800

    saving changes b4 merge

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-#include "graphics.h"
+//#include "graphics.h"
 #include "tinyRandomMap.h"
 #include "agent.h"
 
 int main(int argc, char **argv) {
   unsigned int n = 16;
 
-  Graphics graphics;
+  //Graphics graphics;
   tinyRandomMap map(n, n, 0x0420FEFD, 96);
   Agent * a = new Agent(n, &map);
   a->print_m_map();
