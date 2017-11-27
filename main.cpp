464f70093dcf213961cc94e67542868da5f87478
Hunter Mayers <HunterCMayers@gmail.com>
2017-11-26 18:42:39 -0800

    Updated main to reflect master

--- main.cpp
+++ main.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 
-//#include "graphics.h"
+#include "graphics.h"
 #include "tinyRandomMap.h"
 #include "agent.h"
 
 int main(int argc, char **argv) {
   unsigned int n = 16;
 
-  //Graphics graphics;
+  Graphics graphics;
   tinyRandomMap map(n, n, 0x0420FEFD, 96);
-  Agent * a = new Agent(n, &map);
-  a->traverse_matrix();
+  Agent agent(n, &map);
+  agent.traverse_matrix();
 
+  graphics.Render(&map);
 
-  //graphics.Render(&map);
-
-  //SDL_Delay(10000);
+  SDL_Delay(10000);
 
   return 0;
 }
